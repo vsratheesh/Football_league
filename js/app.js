7204bd7822ec29fed42c119914b67ce68c309971
vsratheesh <vsratheesh95@gmail.com>
2016-05-31 17:58:29 +0530

    added controller

--- js/app.js
+++ js/app.js
@@ -5,16 +5,43 @@ angularApp.config(function ($routeProvider){
   .when ('/',{
     templateUrl : 'templates/Home.html',
     controller: 'HomeController',
+    controllerAs : 'hc',
   })
-  .when('/',{
-  	templateUrl : 'templates/Leagues.html',
-  	controller : 'LeaguesController',
-  })
-  .when('/',{
-  	templateUrl : 'templates/Matches.html',
-  	controller : 'MatchesController',
-  })
-  .when('/',{
-  	templateUrl : 'templates/Team.html',
-  	controller : 'TeamController'
-  })
+})
+  // .when('/',{
+  // 	templateUrl : 'templates/Leagues.html',
+  // 	controller : 'LeaguesController',
+  // 	controllerAs : 'lc',
+  // })
+  // .when('/',{
+  // 	templateUrl : 'templates/Matches.html',
+  // 	controller : 'MatchesController',
+  // 	controllerAs : 'mc',
+  // })
+  // .when('/',{
+  // 	templateUrl : 'templates/Team.html',
+  // 	controller : 'TeamController'
+  // 	controllerAs : 'tc',
+  // })
+
+
+
+angularApp.controller("HomeController",['$resource','$http',function($resource,$filter,$routeParams){
+//    var vm = this;
+//    var HomeDetails = $resource('http://api.football-data.org/v1/soccerseasons', {}, {
+//     get: {
+//         method: 'GET',
+//         headers: { 'X-Auth-Token' : '4f2de580e2a84e1a8c7f6714d2fbef17' }
+//     }
+// });
+//    // $httpProvider.defaults.headers.get = { 'X-Auth-Token' : '4f2de580e2a84e1a8c7f6714d2fbef17' }
+//    // vm.response = HomeDetails.get('http://api.football-data.org/v1/soccerseasons');
+//    console.log(HomeDetails);
+        var User = $resource('http://api.football-data.org/v1/soccerseasons', { }, {
+            query: {
+                method: 'GET',
+                headers: { 'X-Auth-Token': '4f2de580e2a84e1a8c7f6714d2fbef17'}
+            }
+        });
+        console.log(User);
+}])
