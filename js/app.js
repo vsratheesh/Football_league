8df101969fe0aa46d6eb7412fe17b8ad08a1f618
vsratheesh <vsratheesh95@gmail.com>
2016-06-01 11:48:26 +0530

    landing page done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 var angularApp = angular.module('Football_Leauges', ['ngResource','ngRoute']);
 
-angularApp.config(function ($routeProvider){
-  $routeProvider
-  .when ('/',{
-    templateUrl : 'templates/Home.html',
-    controller: 'HomeController',
-    controllerAs : 'hc',
-  })
+angularApp.config(function ($routeProvider,$httpProvider){
+	$httpProvider.defaults.headers.get = { 'X-Auth-Token' : '4f2de580e2a84e1a8c7f6714d2fbef17'}
+	$routeProvider
+	.when ('/',{
+		templateUrl : 'templates/Home.html',
+		controller: 'HomeController',
+		controllerAs : 'hc'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 })
   // .when('/',{
   // 	templateUrl : 'templates/Leagues.html',
@@ -26,22 +30,30 @@ angularApp.config(function ($routeProvider){
 
 
 
-angularApp.controller("HomeController",['$resource','$http',function($resource,$filter,$routeParams){
-//    var vm = this;
+angularApp.controller("HomeController",['$resource','$http',function($resource,$routeParams,$http){
+  var vm=this;
+  var footballResource = $resource('http://api.football-data.org/v1/soccerseasons');
+  vm.football_leauges = footballResource.query();
+  console.log(vm.football_leauges);
+}]);
+
 //    var HomeDetails = $resource('http://api.football-data.org/v1/soccerseasons', {}, {
 //     get: {
 //         method: 'GET',
 //         headers: { 'X-Auth-Token' : '4f2de580e2a84e1a8c7f6714d2fbef17' }
 //     }
 // });
+
 //    // $httpProvider.defaults.headers.get = { 'X-Auth-Token' : '4f2de580e2a84e1a8c7f6714d2fbef17' }
 //    // vm.response = HomeDetails.get('http://api.football-data.org/v1/soccerseasons');
 //    console.log(HomeDetails);
-        var User = $resource('http://api.football-data.org/v1/soccerseasons', { }, {
-            query: {
-                method: 'GET',
-                headers: { 'X-Auth-Token': '4f2de580e2a84e1a8c7f6714d2fbef17'}
-            }
-        });
-        console.log(User);
-}])
+ 
+//         var User = $resource('http://api.football-data.org/v1/soccerseasons', {}, {
+     
+//         });
+//         var data = User.get().$promise.then(function(result){
+//         	console.log(result);
+//         }, function(error){
+//         	console.log('error',error);
+//         })
+// }]);
